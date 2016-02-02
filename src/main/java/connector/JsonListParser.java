d281f110ffc0b86d7dea41e02c7c446b26fa232d
SeriousShit <hagen@serious-shit.de>
2016-02-03 00:01:20 +0100

    add Test

---
+++ src/main/java/connector/JsonListParser.java
@@ -0,0 +1,12 @@
+package connector;
+
+/**
+ * Created by hagen on 02/02/16.
+ */
+public class JsonListParser {
+
+    public static boolean parseList(){
+        return false;
+    }
+
+}
---
+++ src/test/java/connector/JsonListParserTest.java
@@ -0,0 +1,22 @@
+package connector;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hagen on 02/02/16.
+ */
+public class JsonListParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testParseList() throws Exception {
+        assertTrue(true);
+    }
+}
